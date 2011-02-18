1e4b747ce3b9cfb1bcd3ef3459a726d2579d8f05
Dan VerWeire <dverweire@gmail.com>
2011-02-17 19:01:30 -0500

    modified sql.js to include a prompt to make it more obvious that you can entere a sql query.

--- sql.js
+++ sql.js
@@ -1,10 +1,18 @@
 var IsqlConnection = require('./IsqlConnection.js');
 
-var con = new IsqlConnection({ DSN : 'server_dsn_name', userName : 'user_name', password : 'password'});
+var con = new IsqlConnection({ DSN : 'odbc_dsn_name', userName : 'user_name', password : 'password'});
+
+process.stdout.write('> ');
+
+var queryHistory = [];
 
 process.stdin.on('data',function (strInput) {
+	queryHistory.push(strInput);
+	
  	con.query(strInput, function (rs) {
-		console.log(rs.length);
+		console.log(rs.length + ' records returned');
+		
+		process.stdout.write('> ');
 	});
 });
  
